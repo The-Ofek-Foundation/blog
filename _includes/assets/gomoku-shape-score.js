2508f317beea9a0de0905117c4f91187337c79ff
Ofek <ofek@trueskills.com>
2015-12-12 17:08:18 -0800

    Add second blog post!

---
+++ _includes/assets/gomoku-shape-score.js
@@ -0,0 +1,44 @@
+function gomoku_shape_score(consecutive, open_ends, curr_turn) {
+  if (open_ends == 0 && consecutive < 5s)
+    return 0;
+  switch (consecutive) {
+    case 4:
+      switch (open_ends) {
+        case 1:
+          if (curr_turn)
+            return 100000000;
+          return 50;
+        case 2:
+          if (curr_turn)
+            return 100000000;
+          return 500000;
+      }
+    case 3:
+      switch (open_ends) {
+        case 1:
+          if (curr_turn)
+            return 7;
+          return 5;
+        case 2:
+          if (curr_turn)
+            return 10000;
+          return 50;
+      }
+    case 2:
+      switch (open_ends) {
+        case 1:
+          return 2;
+        case 2:
+          return 5;
+      }
+    case 1:
+      switch (open_ends) {
+        case 1:
+          return 0.5;
+        case 2:
+          return 1;
+      }
+    default:
+      return 200000000;
+  }
+}
