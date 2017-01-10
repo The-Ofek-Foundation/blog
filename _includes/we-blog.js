fa1b97a765df10a2f7f3e5b3ac040d9976e54c5d
Ofek Gila <ofek@theofekfoundation.org>
2017-01-09 18:22:24 -0800

    Convert main site jquery to js

--- _includes/we-blog.js
+++ _includes/we-blog.js
@@ -1,18 +1,22 @@
-$(document).ready(function() {
-	$('.title-case').each(function() {
-		$(this).text(toTitleCase($(this).text()));
-	});
-	$('.unhyphenate').each(function() {
-		$(this).text($(this).text().replace(/-/g, ' '));
-	});
+document.addEventListener("DOMContentLoaded", function () {
+	var titleCaseElements = document.getElementsByClassName('title-case');
+	for (var i = 0; i < titleCaseElements.length; i++)
+		titleCaseElements[i].innerHTML = toTitleCase(titleCaseElements[i].innerHTML);
+
+
+	var unhyphenateElements = document.getElementsByClassName('unhyphenate');
+	for (var i = 0; i < unhyphenateElements.length; i++)
+		unhyphenateElements[i].innerHTML = unhyphenateElements[i].innerHTML.replace(/-/g, ' ');
+
 	{% if page.new-tab-redirect %}
-	    $('.page-content').find('a').each(function() {
-	      if (this.href.substring(0, "{{site.url}}{{site.baseurl}}".length) != "{{site.url}}{{site.baseurl}}")
-	        $(this).attr('target', '_blank');
-	    });
-    {% endif %}
+		var links = document.getElementsByTagName('a');
+		for (var i = 0; i < links.length; i++)
+			if (links[i].href.substring(0, "{{site.url}}{{site.baseurl}}".length)
+				!= "{{site.url}}{{site.baseurl}}")
+				links[i].target = '_blank';
+	{% endif %}
 });
 
 function toTitleCase(str) {
-    return str.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	return str.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
