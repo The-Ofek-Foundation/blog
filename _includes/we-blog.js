ccf578a0ccb68f16c9fd1e335b25797005b3a1f2
Ofek Gila <ofek@theofekfoundation.org>
2017-03-15 14:19:21 -0700

    Remove unnecessary cde

--- _includes/we-blog.js
+++ _includes/we-blog.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var titleCaseElements = document.getElementsByClassName('title-case');
-	for (var i = 0; i < titleCaseElements.length; i++)
-		titleCaseElements[i].innerHTML = toTitleCase(titleCaseElements[i].innerHTML);
-
-
-	var unhyphenateElements = document.getElementsByClassName('unhyphenate');
-	for (var i = 0; i < unhyphenateElements.length; i++)
-		unhyphenateElements[i].innerHTML = unhyphenateElements[i].innerHTML.replace(/-/g, ' ');
-
 	{% if page.new-tab-redirect %}
 		var links = document.getElementsByTagName('a');
 		for (var i = 0; i < links.length; i++)
@@ -16,7 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
 				links[i].target = '_blank';
 	{% endif %}
 });
-
-function toTitleCase(str) {
-	return str.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
